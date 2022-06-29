eed39f3bb7a324eb64140266d6bd1bbdad1646cc
fnicoluzzi <fnicoluzzi@gmail.com>
2022-06-29 00:34:29 -0300

    Teste para resolver erro 419

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'payment/*', // exclude all URLs with payment/ prefix
-        'user/add' // exclude exact URL
+        'coletas/novo' // exclude exact URL
+        'retiras/novo' // exclude exact URL
     ];
 }
